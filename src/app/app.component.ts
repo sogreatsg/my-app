0a8093f6f4872e2da466f0d4243c75722acfea1c
thanapol <thanapols.ts@gmail.com>
2021-11-18 19:53:38 +0700

    add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Element } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  usersList: AngularFireList<any>;
+  users :any[] = [];
+  usersFilter:any = [];
+  filterValue:any
+  
+  
   constructor(db: AngularFireDatabase) {
-    db.list('users').valueChanges().forEach(element => {
-      console.log(element)
-    });;
-   
+    this.usersList = db.list('users');
+  }
+
+  displayedColumns: string[] = ['Name', 'E-mail', 'Address','Phone'];
+
+  applyFilter(event: Event) {
+    this.filterValue = (event.target as HTMLInputElement).value;
+    this.usersFilter = this.users.filter(x=>
+      x.name.toLowerCase().includes(this.filterValue.trim().toLowerCase())
+    )
+  }
+  ngOnInit(): void {
+
+    this.usersList.valueChanges().subscribe(items => {
+      this.users = items;
+      if(this.filterValue){
+        this.usersFilter = this.users.filter(x=>
+          x.name.toLowerCase().includes(this.filterValue.trim().toLowerCase())
+        )
+      }else{
+        this.usersFilter = items
+      }
+      
+      });
+     
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   declarations: [ AppComponent ],
