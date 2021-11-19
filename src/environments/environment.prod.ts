da56bc8143bb4192f3318625d7e9a786804a4dce
KITTISAK <kittisak.sridet@icloud.com>
2021-11-19 13:33:17 +0700

    update firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCaKaNJqke_254rAbmqBp2X1f97oU4J6KU",
+    authDomain: "homeisolationo2auth.firebaseapp.com",
+    databaseURL: "https://homeisolationo2auth-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "homeisolationo2auth",
+    storageBucket: "homeisolationo2auth.appspot.com",
+    messagingSenderId: "660819896284",
+    appId: "1:660819896284:web:b92c81a9c18fa67433d119"
+  }
 };
